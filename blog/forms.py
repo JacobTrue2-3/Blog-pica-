d129d3228e76cbac2362b2ef1e8f6a308ec32cd1
JacobTrue2-3 <trukhanyakov@gmail.com>
2025-07-19 15:16:17 +0700

    добавлен обрабочтик ошибок, изменена форма

---
+++ blog/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+class PostForm(forms.Form):
+    title = forms.CharField(
+        label='Заголовок поста:', 
+        widget=forms.TextInput(attrs={
+            'placeholder': 'максимальная длина 20 символов',
+            'class': 'title-input'
+        })
+    )
+    
+    text = forms.CharField(
+        label='Текст поста:',
+        widget=forms.Textarea(attrs={
+            'rows': 3 
+        })
+    )
+    
+    def clean_title(self):
+        """Кастомная валидация заголовка"""
+        title = self.cleaned_data['title'].strip()
+        
+        if len(title) > 20:
+            raise ValidationError(
+                'Превышена максимальная длина заголовка (20 символов). '
+                f'Введено {len(title)} символов.'
+            )
+        return title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def get_post_list(request):
@@ -8,38 +9,74 @@ def get_post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+# def get_post_detail(request, post_id):
+#     #post = Post.objects.get(id=post_id) #другой вариант
+#     post = get_object_or_404(Post, id=post_id)
+
+
+#     context = {'post': post}
+
+#     return render(request, 'blog/post_detail.html', context)
+
+# def get_post_add(request):
+#     if request.method == 'GET':
+#         form = PostForm()
+#         return render(request, 'blog/post_add.html', {'form': form})
+
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+
+#         if form.is_valid():
+#             post = Post.objects.create(
+#                 title=form.cleaned_data['title'], 
+#                 text=form.cleaned_data['text'])
+            
+#             return redirect('post_detail', post_id=post.id)
+
+
 def get_post_detail(request, post_id):
-    #post = Post.objects.get(id=post_id) #другой вариант
     post = get_object_or_404(Post, id=post_id)
-
-
     context = {'post': post}
-
     return render(request, 'blog/post_detail.html', context)
 
 def get_post_add(request):
-    if request.method == 'GET':
-        return render(request, 'blog/post_add.html')
-    
     if request.method == 'POST':
-        title =request.POST.get('title').strip()
-        text = request.POST.get('text').strip()
-
-        errors = {}
-
-        if not title:
-            errors['title'] = 'Заполните поле заголовка'
-        if not text:
-            errors['text'] = 'Заполните поле текста'
-
-        if not errors:
-            post = Post.objects.create(title=title, text=text)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = Post.objects.create(
+                title=form.cleaned_data['title'], 
+                text=form.cleaned_data['text']
+            )
             return redirect('post_detail', post_id=post.id)
-        else:
-            context = {
-                'errors': errors,
-                'title': title,
-                'text': text
-            }
-            return render(request, 'blog/post_add.html', context=context)
+        # Если форма невалидна, продолжит выполнение и отобразит форму с ошибками
+    else:
+        form = PostForm()
+    
+    return render(request, 'blog/post_add.html', {'form': form})
+
+# def get_post_add(request):
+#     if request.method == 'GET':
+#         return render(request, 'blog/post_add.html')
+    
+#     if request.method == 'POST':
+#         title =request.POST.get('title').strip()
+#         text = request.POST.get('text').strip()
+
+#         errors = {}
+
+#         if not title:
+#             errors['title'] = 'Заполните поле заголовка'
+#         if not text:
+#             errors['text'] = 'Заполните поле текста'
+
+#         if not errors:
+#             post = Post.objects.create(title=title, text=text)
+#             return redirect('post_detail', post_id=post.id)
+#         else:
+#             context = {
+#                 'errors': errors,
+#                 'title': title,
+#                 'text': text
+#             }
+#             return render(request, 'blog/post_add.html', context=context)
 
