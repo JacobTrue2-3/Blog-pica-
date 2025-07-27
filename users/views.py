cb72384a73e17a6da9c701b75dfc56fdb2a2f155
JacobTrue2-3 <trukhanyakov@gmail.com>
2025-07-27 16:02:46 +0700

    Добавлены кнопки авторизации и выхода пользователя

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 def register_view(request):
@@ -8,7 +9,7 @@ def register_view(request):
 
         if form.is_valid():
             form.save()
-            return redirect('post_list') #поменять на профиль 
+            return redirect('Login') #поменять на профиль 
         else:
             return render(request, 'users/register.html', {'form': form})
     form = UserCreationForm
@@ -16,3 +17,34 @@ def register_view(request):
     return render(request, 'users/register.html', {'form': form})
     
 
+# def login_view(request):
+#     if request.method == "POST":
+#         form = AuthenticationForm(request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+
+#             return redirect('post_list')
+#         else:
+#             return render(request, 'users/login.html', {'form':form})
+#     form = AuthenticationForm()
+
+#     return render(request, 'users/login.html', {'form':form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)  # <-- Исправлено здесь
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('post_list')
+        else:
+            return render(request, 'users/login.html', {'form': form})
+    
+    form = AuthenticationForm(request)  # <-- И здесь добавили request
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('post_list')
