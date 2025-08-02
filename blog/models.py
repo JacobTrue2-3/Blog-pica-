af2153020cf99e0357c339a95e03fedb4f7ef3f6
JacobTrue2-3 <trukhanyakov@gmail.com>
2025-08-02 16:37:06 +0700

    Добавлено поле автора в модель пост

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import User
+
+
+User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name= "Заголовок") 
     text = models.TextField(verbose_name= "Текст")
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name="Автор") #SET_NULL позволяет удалить пользователя, но посты останутся
 
     class Meta:
         verbose_name= "Пост"
