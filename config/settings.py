c0dd83aaca9e85903d231c74eb4c65aeafab126f
JacobTrue2-3 <trukhanyakov@gmail.com>
2025-08-03 16:00:04 +0700

    Добавлен профиль пользователя с отображением постов и изменена логика перенаправления после входа в систему, добавлены ссылки перехода на посты и профили пользователей

--- config/settings.py
+++ config/settings.py
@@ -128,4 +128,4 @@ STATICFILES_DIRS = [BASE_DIR / 'static/']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'post_list'
+LOGIN_REDIRECT_URL = 'profile'
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, get_user_model
+
 from config.settings import LOGIN_REDIRECT_URL
+from blog.models import Post
+
+
+User = get_user_model()
 
 def register_view(request):
     if request.method == 'POST':
@@ -28,14 +33,27 @@ def login_view(request):
             # при попытке входа, если он есть
             # Если next не указан, то будет использоваться LOGIN_REDIRECT_URL из настроек
             next_url = request.GET.get('next', LOGIN_REDIRECT_URL)  # '/post/add/'
-            return redirect(next_url)
+            if next_url == LOGIN_REDIRECT_URL:
+                return redirect(next_url, request.user.username)  # Переход на профиль пользователя
+            else: return redirect(next_url)
         else:
             return render(request, 'users/login.html', {'form': form})
     
-    form = AuthenticationForm(request)  # <-- И здесь добавили request
+    form = AuthenticationForm(request)  
     return render(request, 'users/login.html', {'form': form})
 
 
 def logout_view(request):
     logout(request)
     return redirect('post_list')
+
+def user_profile(request, user_username):
+    user = get_object_or_404(User, username=user_username)
+    posts = Post.objects.filter(author=user).order_by('-created_at')
+
+    context = {
+        'user': user,
+        'posts': posts,
+    }
+
+    return render(request, 'users/profile.html', context)
