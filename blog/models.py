d620b3c7cae726270e7e2804f34a9f5245cc2948
JacobTrue2-3 <trukhanyakov@gmail.com>
2025-08-29 11:45:41 +0700

    Добавлено поле изображения в модель поста, обновлены настройки для загрузки медиафайлов и изменен шаблон для отображения изображения поста.

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,8 @@ User = get_user_model()
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name= "Заголовок") 
     text = models.TextField(verbose_name= "Текст")
-    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='posts/', null=True, blank=True, verbose_name="Изображение")
+    created_at = models.DateTimeField(auto_now_add=True) 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name="Автор") #SET_NULL позволяет удалить пользователя, но посты останутся
 
     class Meta:
