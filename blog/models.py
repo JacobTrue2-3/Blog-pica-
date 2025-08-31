b6560ddf7acfa12932c5b1580f3dd75b2214138b
JacobTrue2-3 <trukhanyakov@gmail.com>
2025-08-31 15:11:59 +0700

    Обновлены поля created_at и updated_at в модели Post для добавления русскоязычных меток "Дата создания" и "Дата обновления". Создана миграция для применения этих изменений.

--- blog/models.py
+++ blog/models.py
@@ -20,8 +20,8 @@ class Post(models.Model):
     tags = models.ManyToManyField('Tag', related_name='posts', blank=True, verbose_name="Теги")
     text = models.TextField(verbose_name= "Текст")
     image = models.ImageField(upload_to='posts/', null=True, blank=True, verbose_name="Изображение")
-    created_at = models.DateTimeField(auto_now_add=True) 
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания") 
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name="Автор") #CASCADE позволяет удалить пользователя, но посты останутся, SET_NULL позволяет удалить пользователя, но посты останутся
     status = models.CharField(choices=STATUS_CHOICES, default='draft', verbose_name="Статус")
 
